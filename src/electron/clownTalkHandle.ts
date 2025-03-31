ee8791c9530ee5987740256644410481429a5b7b
Melquiceded <melqui4251@gmail.com>
2025-03-31 02:18:28 -0400

    Ready to start working on the front end after getting everything ready on the clownTalk protocol 	modified:   src/electron/clownTalkHandle.ts

--- src/electron/clownTalkHandle.ts
+++ src/electron/clownTalkHandle.ts
@@ -1,16 +1,42 @@
+import { rejects } from "assert";
+import { error } from "console";
+import { stat } from "fs";
 import { resolve } from "path";
 
+const invalid_method =
+    {
+        status:'FAIL',
+        message:'INVALID clownTalk COMUNICATION METHODFAIL',
+    }
 
 
 export function processTalk(obj:any):Promise<any>
 {
     return new Promise<any>((resolve)=>
     {
-        let data = {
-            Message:"OK",
-            Received:obj
-
+        let data = 
+        {
+            status:'OK',
+            message:{}
         }
-        resolve(data);
+         if(typeof obj !== 'object' || obj === null)
+         {
+                  resolve(invalid_method);
+            return;
+         }
+         
+         switch(obj.type)
+         {
+            case 'search-video':
+                
+            default:
+                resolve(invalid_method);
+                return;
+
+         }
+        
+         resolve(data);
+       
+       
     });
 }
