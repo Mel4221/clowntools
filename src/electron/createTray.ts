789e3e882a7b84542fe830901b22b0dc30b0bcfd
Melquiceded <melqui4251@gmail.com>
2025-03-31 22:36:47 -0400

    Setting up the final adjustments for the default template. 	modified:   electron-builder.json 	modified:   index.html 	deleted:    public/img.jpg 	deleted:    public/react.svg 	deleted:    public/vite.svg 	renamed:    public/setting.png -> src/electron/assets/icon.png 	modified:   src/electron/createTray.ts 	renamed:    src/electron/assets/setting.png -> src/ui/assets/icon.png

--- src/electron/createTray.ts
+++ src/electron/createTray.ts
@@ -5,7 +5,7 @@ import { BrowserWindow ,Tray,Menu,app} from "electron";
 export function createTray(mainWindow: BrowserWindow)
 {
        const tray = new Tray(
-            "src/electron/assets/setting.png"
+            "src/electron/assets/icon.png"
             /*
             path.join(getAssetPath(),
             process.platform === 'darwin' ? 'setting.png':'setting.png'
